6c8a4a0552c89c8875f6f1e4a77d9b2cfaf5e227
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-11-28 06:39:15 +0700

    feat: connect routes

--- payment-app/src/index.ts
+++ payment-app/src/index.ts
@@ -1,17 +1,35 @@
 import express, { Request, Response } from 'express';
-import { PaymentController } from './controllers/payment.controller';
 import * as dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { auth } from './middleware/auth';
+import { create, del, get, update } from './routes/invoice-routes';
+import { createInvoice, pay } from './routes/payment-routes';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PAYMENT_SERVICE_PORT as string;
+const port = Number(process.env.PAYMENT_SERVICE_PORT);
+app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
-});
+app.use(
+  cors({
+    origin: ['*'],
+    credentials: true,
+    exposedHeaders: ['Authorization', 'x-api-key'],
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+  }),
+);
 
-const pc = new PaymentController();
+app.use('/api', auth);
+
+app.get('/api/invoices/:booking_id', get);
+app.post('/api/invoices', create);
+app.put('/api/invoices/:booking_id', update);
+app.delete('/api/invoices/:booking_id', del);
+app.post('/api/payment', createInvoice);
+
+app.get('/payment', pay);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
