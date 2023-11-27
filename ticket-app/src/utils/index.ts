8d7691ae122793783740fa4d2b282b82182e721d
debbyalmadea <almadeaputri@gmail.com>
2023-11-27 12:36:35 +0700

    fix: docker compose and delete protobuf

--- ticket-app/src/utils/index.ts
+++ ticket-app/src/utils/index.ts
@@ -1,38 +1,7 @@
-import { SeatStatus } from "@/proto/com/ticket_app/v1/seats_pb";
 import tryCatchWrapper from "./tryCatchWrapper"
 import JsonResponse from "./JsonResponse"
 import errorHandlerChain from './ErrorHandler';
 import HttpError from "./HttpError";
-
-export function mapStringToSeatStatus(status: 'available' | 'booked' | 'sold'): SeatStatus {
-   const statusMap = {
-            available: SeatStatus.AVAILABLE,
-            booked: SeatStatus.BOOKED,
-            sold: SeatStatus.SOLD,
-    }
-    
-    const seatStatus = statusMap[status] as SeatStatus;
-    if (!seatStatus) {
-        throw new Error(`Invalid seat status: ${status}`);
-    }
-
-    return seatStatus;
-}
-
-export function mapSeatStatusToString(status: SeatStatus): 'available' | 'booked' | 'sold' {
-   const statusMap = {
-         [SeatStatus.AVAILABLE]: 'available',
-         [SeatStatus.BOOKED]: 'booked',
-         [SeatStatus.SOLD]: 'sold',
-   }
-
-    const stringStatus =  statusMap[status] as 'available' | 'booked' | 'sold';
-    if (!stringStatus) {
-        throw new Error(`Invalid seat status: ${status}`);
-    }
-
-    return stringStatus;
-}
 
 export {
     tryCatchWrapper,
