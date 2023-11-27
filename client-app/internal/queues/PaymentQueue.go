b89f9b0b7f56eb0886a8df3a3d42add0bbf0a488
Rava Maulana <ravamaulana14@gmail.com>
2023-11-27 18:39:22 +0700

    feat: added env for rabbitmq

--- client-app/internal/queues/PaymentQueue.go
+++ client-app/internal/queues/PaymentQueue.go
@@ -1,25 +1,53 @@
 package queues
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	booking_controller "github.com/ashnchiquita/if4031-ticketing-and-reservation-services/internal/controllers/booking"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func InitPaymentQueue() {
-	conn, err := amqp.Dial("amqp://guest:guest@tubes-message-broker:5672/")
+	tryAttempt := 0
+
+	user := os.Getenv("MESSAGE_BROKER_USER")
+	pass := os.Getenv("MESSAGE_BROKER_PASSWORD")
+	port := os.Getenv("MESSAGE_BROKER_PORT")
+
+	dsn := fmt.Sprintf("amqp://%s:%s@tubes-message-broker:%s/", user, pass, port)
+
+	conn, err := amqp.Dial(dsn)
+	for err != nil && tryAttempt < 5 {
+		log.Printf("[RETRYING] Failed to connect to RabbitMQ: %s", err.Error())
+		time.Sleep(10 * time.Second)
+		conn, err = amqp.Dial(dsn)
+		tryAttempt++
+	}
+
 	if err != nil {
-		log.Panicf("Failed to connect to RabbitMQ: %s", err.Error())
+		log.Panicf("[FAIL] Failed to connect to RabbitMQ: %s", err.Error())
 		return
 	}
+
 	defer conn.Close()
 
+	tryAttempt = 0
 	ch, err := conn.Channel()
+	for err != nil && tryAttempt < 5 {
+		log.Printf("[RETRYING] Failed to open a channel: %s", err.Error())
+		time.Sleep(10 * time.Second)
+		ch, err = conn.Channel()
+		tryAttempt++
+	}
+
 	if err != nil {
-		log.Panicf("Failed to open a channel: %s", err.Error())
+		log.Printf("[FAIL] Failed to open a channel: %s", err.Error())
 		return
 	}
+
 	defer ch.Close()
 
 	queue, err := ch.QueueDeclare("payment_message", true, false, false, false, nil)
